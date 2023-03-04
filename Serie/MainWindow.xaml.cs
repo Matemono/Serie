9adb7a03c5a2dafbf4a4a04f06d719f4baf9ba9f
Mateusz <Mateusz@DESKTOP-KFMV9GE>
2023-03-04 15:03:30 +0100

    Dodaj pliki projektów.

---
+++ Serie/MainWindow.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace Serie
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ObservableCollection<string> lista_cwiczen = new ObservableCollection<string>();
+
+            lista_cwiczen.Add("sztanga");
+            lista_cwiczen.Add("bicki");
+            lista_cwiczen.Add("brzuszki");
+
+            RodzajCwiczenia.ItemsSource= lista_cwiczen;
+
+            Data.Text = dzis.ToString("d");
+
+        }
+        static DateTime dzis = DateTime.Today;
+
+        public String data = dzis.ToString("d");
+        public String rodzaj_cwiczenia = ""; 
+        public int numer_serii = 0;
+        public int liczba_powtorzen = 0;
+        public int waga_obciazenia = 0;
+
+
+        // Rodzaj ćwiczenia
+        private void Rodzaj_cwiczenia(object sender, SelectionChangedEventArgs e)
+        {
+            rodzaj_cwiczenia = RodzajCwiczenia.SelectedItem.ToString(); // zapisuje wybor rodzaju cwiczenia do zmiennej String
+        }
+        
+        private void Numer_serii(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                numer_serii = int.Parse(NumerSerii_wartosc.Text);
+            }
+            catch (Exception)
+            {
+                NumerSerii_wartosc.Clear();
+                //NumerSerii_wartosc.Text = NumerSerii_wartosc.Text.Remove(NumerSerii_wartosc.Text.Length);
+                //MessageBox.Show("Podano zły format dancyh !");
+            }
+                        
+        }
+
+        private void Liczba_powtorzen(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                liczba_powtorzen = int.Parse(LiczbaPowtorzen_wartosc.Text);
+                if(liczba_powtorzen == 0)
+                    LiczbaPowtorzen_wartosc.Clear();
+            }
+            catch (Exception)
+            {
+                LiczbaPowtorzen_wartosc.Clear();
+                //MessageBox.Show("Podano zły format dancyh !");
+            }
+        }
+
+        private void Waga_obciazenia(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                waga_obciazenia = int.Parse(WagaObciazenia_wartosc.Text);
+            }
+            catch (Exception)
+            {
+                WagaObciazenia_wartosc.Clear();
+                //MessageBox.Show("Podano zły format dancyh !");
+            }
+        }
+
+        private void NastepnaSeria_Click(object sender, RoutedEventArgs e)
+        {
+            Seria seria = new Seria(data,rodzaj_cwiczenia,numer_serii,liczba_powtorzen,waga_obciazenia);
+            
+            Seria.Zapisz(seria);
+            MessageBox.Show("Zapisano");
+
+            
+
+            //String rodzaj_cwiczenia_do_bazy_danych = RodzajCwiczenia.SelectedItem.ToString(); // pobiera wybrany rodzaj cwiczenia
+
+            //MessageBox.Show(rodzaj_cwiczenia);
+            /*
+            using (StreamWriter writer = new StreamWriter("Seria.txt"))
+            {
+                writer.Write(numer_serii);
+            }
+            */
+        }
+
+        private void ZobaczSerie_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Serie/Seria.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows;
+
+namespace Serie
+{
+    class Seria
+    {
+        private String data;
+        private String rodzaj_cwiczenia;
+        private int numer_serii;
+        private int liczba_powtorzen;
+        private int waga_obciazenia;
+
+        public Seria(String data, String rodzaj_cwiczenia, int numer_serii, int liczba_powtorzen, int waga_obciazenia) 
+        {
+            this.data = data;
+            this.rodzaj_cwiczenia = rodzaj_cwiczenia;
+            this.numer_serii = numer_serii;
+            this.liczba_powtorzen = liczba_powtorzen;
+            this.waga_obciazenia = waga_obciazenia;
+        }
+
+        public static async Task Zapisz(Seria seria)
+        {
+            String linia_do_zapisu = seria.data + " " + seria.rodzaj_cwiczenia + " " + seria.numer_serii + " " + seria.liczba_powtorzen + " " + seria.waga_obciazenia + " ";
+            using StreamWriter file = new("Serie_cwiczen.txt", append: true);
+            await file.WriteLineAsync(linia_do_zapisu);
+            
+        }
+    }
+}
